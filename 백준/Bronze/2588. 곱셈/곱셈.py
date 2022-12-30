1d2323fe6db8cdaf798ce370a7d77e051bcb2c3c
aarom416 <106041072+aarom416@users.noreply.github.com>
2022-12-30 20:28:56 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,17 @@
+import sys
+
+sum = [0,0,0]
+total = 0
+
+first_num = int(sys.stdin.readline().strip())
+second_num = list(map(int, sys.stdin.readline().strip()))
+
+for i in reversed(range(3)) : 
+    print(f"{first_num*second_num[i]}")
+    sum[2-i] = (first_num*second_num[i])
+
+for i in range(3) :
+    total = total + sum[i]*(10**i)
+print(total)
+    
+    
