7478a7831a01157022ee8cf265700abf2f44c8ad
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-01-09 20:06:49 +0900

    Update 평균은 넘겠지.py

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -5,11 +5,25 @@ for i in range(test):
     score = []
     n = list(map(int,input().split()))
     for j in range(1,len(n)):
-        score.append(n[j])
+        score.append(n[j]) # 첫번쨰 학생수를 제외한 리스트
     avg = sum(score)/len(score)
 
     for i in score:
         if i > avg:
             cnt += 1
     print(f"{cnt/len(score)*100:.3f}%")
+    
+#다른 문제 풀이
+
+n = int(input())
+
+for _ in range(n):
+    nums = list(map(int, input().split()))
+    avg = sum(nums[1:])/nums[0]  # 평균을 구함 (nums[0]: 학생수, nums[1:] 점수)
+    cnt = 0
+    for score in nums[1:]:
+        if score > avg:
+            cnt += 1  # 평균 이상인 학생 수
+    rate = cnt/nums[0] *100
+    print(f'{rate:.3f}%')
 
