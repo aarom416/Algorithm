a47ae096128c8757c6ab6986c54189cd02987bac
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-01-11 15:09:07 +0900

    [Bronze I] Title: 단어 공부, Time: 88 ms, Memory: 32416 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,13 +1,12 @@
-s = input()
+s = input().upper()
 string_cnt = []
 
-string = list(s.upper())
-for i in set(string):  # set을 사용해 중복제거 후 루프
-    string_cnt.append(string.count(i))  # 각 문자 개수 구함
+string = list(set(s))
+for i in string:  # set을 사용해 중복제거 후 루프
+    string_cnt.append(s.count(i))  # 각 문자 개수 구함
 
 if string_cnt.count(max(string_cnt)) > 1:  # 최대값이 2개 이상인 경우
     print("?")
 else:
-    max_idx = string_cnt.index(max(string_cnt))  # 최대값 인덱스 저장
-    strings = list(set(string))  # 인덱스 string_cnt와 string 맞추기 위해 string을 set으로 중복 제거
-    print(strings[max_idx])
+    max_idx = string_cnt.index(max(string_cnt))  # 최대값 인덱스 저장# 인덱스 string_cnt와 string 맞추기 위해 string을 set으로 중복 제거
+    print(string[max_idx])
