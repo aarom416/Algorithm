81dc0c4a3418ac3a7864113b1aa6585c909a4362
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-01-11 15:42:32 +0900

    Update 상수.py

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -5,4 +5,10 @@ for i in a,b:
     d = (i-c*100)//10
     e = i%10
     change_num.append(e*100+d*10+c)
-print(max(change_num))
+print(max(change_num))'
+
+# 다른 풀이
+a, b = input().split()
+a = int(a[::-1]) #문자열[::-1] 하면 역순
+b = int(b[::-1]) 
+print(max(a, b))
