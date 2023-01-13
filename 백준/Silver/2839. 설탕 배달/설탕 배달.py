c130171be17792e4f5c595b1c09bc4813062a2ca
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-01-13 19:30:11 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+cnt = 0
+while n>=0:
+    if n%5==0:
+        cnt+=(n//5)
+        print(cnt)
+        break
+    n-=3
+    cnt+=1
+    if n<0:
+        print("-1")
+        break
