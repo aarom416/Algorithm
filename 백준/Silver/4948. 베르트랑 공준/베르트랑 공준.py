95c9cdf9d3c494bd5532a2820d52cda5c6ebcab1
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-01-16 00:57:05 +0900

    [Silver II] Title: 베르트랑 공준, Time: 1112 ms, Memory: 39720 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,10 +1,13 @@
 import sys
-def sosu(num):
+def sosu(num): #소수를 검증하는 함수
     for i in range(2,int(num**0.5)+1):
-        if num%i==0:
+        if num%i==0: #나머지가 0이면 소수가 아니므로 False 리턴
             return False
-    return True
+    return True #반복문이 끝나면 소수이므로 True 리턴 
 
+#시간 초과를 해결하기 위해 대신 문제에서 주어진 범위의 2n까지 미리 소수를 구함
+#구한 소수를 리스트에 넣어둠 계산
+#미리 계산하기 때문에 시간 절약
 number = list(range(2,246912))
 sosu_list = []
 for i in number:
