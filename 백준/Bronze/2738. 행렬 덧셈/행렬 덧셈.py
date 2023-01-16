700b06567f62520d94304ce4116dbc8b5d69e06f
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-01-16 18:41:18 +0900

    [Bronze V] Title: 행렬 덧셈 (다른 풀이 포함)

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -12,3 +12,18 @@ for row in range(n):
     for col in range(m):
         print(a[row][col]+b[row][col], end=" ")
     print()
+
+# 다른 풀이
+
+n,m = map(int,input().split())
+a=[0 for _ in range(m) for _ in range(n)]
+b=[0 for _ in range(m) for _ in range(n)]
+for i in range(n):
+        a[i] = list(map(int,input().split()))
+for i in range(n):
+        b[i] = list(map(int,input().split()))
+for i in range(n):
+    for j in range(m):
+        a[i][j]+=b[i][j]
+        print(a[i][j],end=" ")
+    print()
