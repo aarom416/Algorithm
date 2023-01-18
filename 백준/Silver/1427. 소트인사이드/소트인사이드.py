ab7ba7ed712ebf142bf00a7e11e6e2ebdc0d8a54
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-01-18 17:07:38 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 30616 KB

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -3,7 +3,6 @@ n = sys.stdin.readline().rstrip()
 num_list = []
 for i in n:
     num_list.append(i)
-num_list = list(map(int, num_list))
 num_list.sort(reverse=True)
 for i in num_list:
     print(i,end="")
