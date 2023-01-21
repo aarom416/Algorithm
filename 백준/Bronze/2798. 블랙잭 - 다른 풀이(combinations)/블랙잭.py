736154a9eb828440b0a77328faa8053fbe271373
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-01-21 18:40:17 +0900

    [Bronze II] Title: 블랙잭, Time: 100 ms, Memory: 36840 KB

--- 백준/Bronze/2798. 블랙잭 - 다른 풀이(combinations)/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭 - 다른 풀이(combinations)/블랙잭.py
@@ -10,3 +10,15 @@ for i in range(0,len(card_list)):
             else:
                 result.append(card_list[i]+card_list[j]+card_list[k])
 print(max(result))                
+
+#다른 풀이
+
+from itertools import combinations #combinations 함수 사용
+card_num, target_num = map(int, input().split())
+card_list = list(map(int,input().split()))
+biggest_num=0
+for cards in combinations(card_list,3): #입력값을 3개의 모든 조합 만듬
+    temp_sum = sum(cards)
+    if biggest_num<temp_sum<=target_num: #제일 목표값과 가까운 값 찾기
+        biggest_num = temp_sum         
+print(biggest_num)     
