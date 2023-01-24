ae6cb8ee02bd4774599e8b883c758fac845b2fb9
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-01-24 18:25:31 +0900

    Update 단어 뒤집기.py

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -2,7 +2,7 @@ import sys
 n = int(sys.stdin.readline())
 reverse_list=[]
 for _ in range(n):
-    word_list = sys.stdin.readline().split()
+    word_list = sys.stdin.readline().split() #리스트로 사용
     for i in word_list:
         a=''
         word = list(i) #각 문자를 리스트화
@@ -11,3 +11,18 @@ for _ in range(n):
             a+=j
         print(a,end=" ")
     print()
+
+# 다른 풀이 (stack 사용)
+n = int(input())
+for i in range(n):
+    string=input() #문자열로 사용
+    string+=" "
+    stack=[]
+    for j in string:
+        if j!=" ":
+            stack.append(j)
+        else:
+            while stack:
+                print(stack.pop(),end="")
+            print(' ',end="")
+
