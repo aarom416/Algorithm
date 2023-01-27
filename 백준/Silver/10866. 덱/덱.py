91388da0c376028c3372a61c3c3e8193535fd625
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-01-27 21:50:42 +0900

    [Silver IV] Title: 덱, Time: 72 ms, Memory: 34132 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,37 @@
+from collections import deque
+import sys
+d = deque()
+n = int(sys.stdin.readline().rstrip())
+for _ in range(n):
+    command = sys.stdin.readline().split()
+    if command[0]=='push_front':
+        d.appendleft(command[1])
+    elif command[0]=='push_back':
+        d.append(command[1])
+    elif command[0]=='pop_front':
+        if d:
+            print(d.popleft())
+        else:
+            print(-1)
+    elif command[0]=='pop_back':
+        if d:
+            print(d.pop())
+        else:
+            print(-1)    
+    elif command[0]=='size':
+        print(len(d))
+    elif command[0]=='empty':
+        if d:
+            print(0)
+        else:
+            print(1)
+    elif command[0]=='front':
+        if d:
+            print(d[0])
+        else:
+            print(-1)
+    else:
+        if d:
+            print(d[-1])
+        else:
+            print(-1)
