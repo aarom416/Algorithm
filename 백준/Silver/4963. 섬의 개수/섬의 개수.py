4173b5b503f23ee56917143313c419df0f2e3b86
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-03-08 21:02:13 +0900

    Update 섬의 개수.py

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -28,3 +28,37 @@ while True:
             if dfs(i,j):
                 cnt+=1
     print(cnt)
+    
+#다른 풀이 - bfs
+from collections import deque
+import sys
+sys.setrecursionlimit(10**6) #파이썬은 재귀깊이가 1000으로 설정되있으므로 그 이상 재귀호출이 일어날 수 있게 설정해줌
+input=sys.stdin.readline
+def bfs(x,y):
+    q=deque()
+    q.append((x,y))
+    while q:
+        x,y=q.popleft()
+        for i in range(8):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if nx<0 or nx>=n or ny<0 or ny>=m: #밖으로 나가면 continue
+                continue
+            if graph[nx][ny]==1:
+                graph[nx][ny]=0
+                q.append((nx,ny))
+    
+while True:
+    m,n=map(int,input().split())
+    if n==0 and m==0:
+        break
+    graph=[list(map(int,input().split())) for _ in range(n)]
+    dx=[-1,-1,-1,0,0,1,1,1] #상하좌우 및 대각선 표시
+    dy=[-1,0,1,-1,1,-1,0,1]
+    cnt=0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j]==1:
+                bfs(i,j)
+                cnt+=1
+    print(cnt)
