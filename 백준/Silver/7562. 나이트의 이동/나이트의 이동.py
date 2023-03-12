abf66c25629a3f6d027952c07879b83f11ebd9fb
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-03-12 14:28:16 +0900

    [Silver I] Title: 나이트의 이동, Time: 1316 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,28 +1,29 @@
-
 from collections import deque
 import sys
-input = sys.stdin.readline
-dx = [-1, -2, -2, -1, 1, 2, 2, 1]
-dy = [2, 1, -1, -2, -2, -1, 1, 2]
-def bfs(sx, sy, ax, ay):
-    q = deque()
-    q.append([sx, sy])
-    s[sx][sy] = 1
+input=sys.stdin.readline
+dx=[-1,-2,-2,-1,1,2,2,1]
+dy=[2,1,-1,-2,-2,-1,1,2]
+def bfs(s_x,s_y,e_x,e_y):
+    q=deque()
+    q.append((s_x,s_y))
     while q:
-        a, b = q.popleft()
-        if a == ax and b == ay:
-            print(s[ax][ay] -1)
+        x,y=q.popleft()
+        if x==e_x and y==e_y:
+            print(graph[x][y]-1) #맨 처음 위치값이 1부터 시작했으므로 -1 해줌
             return
         for i in range(8):
-            x = a + dx[i]
-            y = b + dy[i]
-            if 0 <= x < n and 0 <= y < n and s[x][y] == 0:
-                q.append([x, y])
-                s[x][y] = s[a][b] + 1
-t = int(input())
-for i in range(t):
-    n = int(input())
-    sx, sy = map(int, input().split())
-    ax, ay = map(int, input().split())
-    s = [[0] * n for i in range(n)]
-    bfs(sx, sy, ax, ay)
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if nx<0 or nx>=n or ny<0 or ny>=n:
+                continue
+            if graph[nx][ny]==0:
+                graph[nx][ny]=graph[x][y]+1
+                q.append((nx,ny))
+k=int(input())
+for i in range(k):
+    n=int(input())
+    graph=[[0]*n for _ in range(n)]
+    start_x,start_y=map(int,input().split())
+    end_x,end_y=map(int,input().split())
+    graph[start_x][start_y]=1
+    bfs(start_x,start_y,end_x,end_y)
