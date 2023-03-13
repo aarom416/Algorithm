c07dcc557374380ea3fad67755a7560fe95094be
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-03-14 01:27:50 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 80 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.py
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.py
@@ -1,66 +1,62 @@
-from collections import deque
 import sys
-sys.setrecursionlimit(10**9)
+from collections import deque
 input = sys.stdin.readline
 
-n = int(input())
+# 사이클에 속한 노드부터 BFS 탐색으로 나머지 노드의 거리를 구함
+def BFS():
+    que = deque()
 
-graph = [[] for _ in range(n+1)]
+    for i in range(N):
+        if parents[i] == -1:
+            answer[i] = 0
+            que.append(i)
 
-cycle = set()
+    while que:
+        current = que.popleft()
 
-for _ in range(n):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+        for child in graph[current]:
+            if answer[child] == -1:
+                answer[child] = answer[current] + 1
+                que.append(child)
 
-visited = [False] * (n + 1)
 
-cy = False
+N = int(input())
+# 입력받은 그래프 저장
+graph = [[] for _ in range(N)]
+# graph는 추후 BFS 탐색때 사용해야하므로, 노드를 제거할 별도의 그래프
+temp = [[] for _ in range(N)]
+# 간선 수를 저장할 변수
+graph_size = [0] * N
+# 부모 노드를 저장할 변수
+parents = [-1] * N
+# 출력될 답을 저장할 변수 (거리)
+answer = [-1] * N
 
-def dfs(start, here, level, li):
-    global cy
-    if level <= 2:
-        for v in graph[here]:
-            if visited[v] == False:
-                visited[v] = True
-                dfs(start, v, level+1, li+[v])
-                visited[v] = False
-    else:
-        for v in graph[here]:
-            if visited[v] == False:
-                visited[v] = True
-                dfs(start, v, level+1, li+[v])
-                visited[v] = False
-            else:
-                if v == start:
-                    cy = True
-                    for l in li:
-                        cycle.add(l)
-                    return
+# 입력 처리
+for _ in range(N):
+    s1, s2 = map(int, input().split())
+    graph[s1-1].append(s2-1)
+    temp[s1-1].append(s2-1)
+    graph_size[s1-1] += 1
+    graph[s2-1].append(s1-1)
+    temp[s2-1].append(s1-1)
+    graph_size[s2-1] += 1
 
-for i in range(1, n+1):
-    if cy == True:
+# 전체 탐색 후 간선 수가 1인 노드가 발견되지 않으면 반복 종료
+while True:
+    flag = True
+    for i in range(N):
+        if graph_size[i] == 1:
+            parent = temp[i].pop()
+            parents[i] = parent
+            temp[parent].remove(i)
+            graph_size[i] = 0
+            graph_size[parent] -= 1
+            flag = False
+
+    if flag:
         break
-    visited[i] = True
-    dfs(i, i, 1, [i])
-    visited[i] = False
 
-cycle = list(cycle)
-# bfs 수행
-answer = [int(1e9)] * (n + 1)
-q = deque()
-for c in cycle:
-    q.append((c, 0))
-    answer[c] = 0
+BFS()
 
-while q:
-    node, level = q.popleft()
-
-    for v in graph[node]:
-        if answer[v] == int(1e9):
-            answer[v] = level+1
-            q.append((v, level+1))
-
-
-print(" ".join(map(str, answer[1:])))
+print(*answer)
