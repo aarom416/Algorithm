50f242a07b955af967eb3bbf7d7cc35f8d4e52a2
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-03-14 16:45:25 +0900

    Update and rename 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.py to 백준/Gold/16947. 서울 지하철 2호선 - 참조(시간초과)/서울 지하철 2호선.py

--- 백준/Gold/16947. 서울 지하철 2호선 - 참조(시간초과)/서울 지하철 2호선.py
+++ 백준/Gold/16947. 서울 지하철 2호선 - 참조(시간초과)/서울 지하철 2호선.py
@@ -1,3 +1,54 @@
+#시간 초과 - 불필요한 재귀 사용으로 인한 것으로 예상됨
+from collections import deque
+import sys
+sys.setrecursionlimit(100000)
+input=sys.stdin.readline
+n=int(input())
+station=[[] for _ in range(n+1)] #역
+visited=[False]*(n+1) 
+cycle_list=[False]*(n+1) #순환선을 담는 리스트
+check=[-1]*(n+1) #결과 리스트
+def dfs(x,s,cnt): #dfs 사용해 순환선 찾기
+    global cycle
+    if cnt>=3 and x==s: # 다음 역이 처음 출발한 역과 같고 3개 이상의 역을 지났다면=순환선이면
+        cycle=True #순환선 표시 후 리턴
+        return
+    visited[x]=True #방문처리
+    for i in station[x]: # 다음 역들 중에서
+        if not visited[i]: #순환선이 아니면 
+            dfs(i,s,cnt+1) #다음 역 재귀 반복
+        elif i==s and cnt>=3: #다음 역이 처음 출발한 역과 같고 3개 이상의 역을 지났다면
+            dfs(i,s,cnt) #cnt를 증가하지 않고 재귀 수행
+def bfs(): #bfs 사용해 순환선까지 거리 찾기(최단 거리)
+    q=deque()
+    for i in range(1,n+1):
+        if cycle_list[i]: #순환역이면
+            check[i]=0  #0으로 처리하여 순환선 구분(순환선 아닌 역은 -1)
+            q.append(i)
+    while q:
+        x=q.popleft()
+        for i in station[x]: #다음 역들 중에서
+            if check[i]==-1: #순환선 아닌 역이면
+                q.append(i)
+                check[i]=check[x]+1 #현재 역의 값은 방금 전역 값+1 해야 순환선까지 거리 구해짐
+    for i in range(1,n+1):
+        print(check[i],end=' ')
+    return
+                    
+for _ in range(n):
+    a,b=map(int,input().split())
+    station[a].append(b)
+    station[b].append(a)
+for i in range(1,n+1):
+    start=i #처음 값 기억
+    cycle=False #순환선인지 확인할 값
+    visited=[False]*(n+1)
+    dfs(i,start,1) 
+    if cycle: #순환선이라면
+        cycle_list[i]=True #해당 역이 순환선임을 표시
+bfs()
+
+#다른 풀이(참조)
 from collections import deque
 import sys
 sys.setrecursionlimit(10000)
