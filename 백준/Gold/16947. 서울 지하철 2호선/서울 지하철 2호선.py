11feb5cc299cde7d3642c8b2432cf20f9b999484
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-03-14 16:39:47 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 80 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.py
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.py
@@ -1,62 +1,45 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
-
-# 사이클에 속한 노드부터 BFS 탐색으로 나머지 노드의 거리를 구함
-def BFS():
-    que = deque()
-
-    for i in range(N):
-        if parents[i] == -1:
-            answer[i] = 0
-            que.append(i)
-
-    while que:
-        current = que.popleft()
-
-        for child in graph[current]:
-            if answer[child] == -1:
-                answer[child] = answer[current] + 1
-                que.append(child)
-
-
-N = int(input())
-# 입력받은 그래프 저장
-graph = [[] for _ in range(N)]
-# graph는 추후 BFS 탐색때 사용해야하므로, 노드를 제거할 별도의 그래프
-temp = [[] for _ in range(N)]
-# 간선 수를 저장할 변수
-graph_size = [0] * N
-# 부모 노드를 저장할 변수
-parents = [-1] * N
-# 출력될 답을 저장할 변수 (거리)
-answer = [-1] * N
-
-# 입력 처리
-for _ in range(N):
-    s1, s2 = map(int, input().split())
-    graph[s1-1].append(s2-1)
-    temp[s1-1].append(s2-1)
-    graph_size[s1-1] += 1
-    graph[s2-1].append(s1-1)
-    temp[s2-1].append(s1-1)
-    graph_size[s2-1] += 1
-
-# 전체 탐색 후 간선 수가 1인 노드가 발견되지 않으면 반복 종료
+import sys
+sys.setrecursionlimit(10000)
+input=sys.stdin.readline
+n=int(input())
+station=[[] for _ in range(n+1)] #현재 역 설정을 위한 리스트
+next_station=[0]*(n+1) #다음 역 설정을 위한 리스트(순환선이 아닌 역)->순환선이면 0으로 처리
+temp=[[] for _ in range(n+1)] #다음 역 설정 처리를 위해 임의로 만든 리스트
+station_size=[0]*(n+1) #각 역의 간선 수를 처리할 리스트
+answer=[-1]*(n+1) #결과 리스트
+def bfs(): #순환선으로부터 떨어진 거리 구하는 bfs
+    q=deque()
+    for i in range(1,n+1): #순환선인 역 찾기
+        if next_station[i]==0: #순환선이면
+            answer[i]=0 #결과 0
+            q.append(i) 
+    while q:
+        x=q.popleft()
+        for i in station[x]:
+            if answer[i]==-1: #순환선이 아닌 역이면
+                answer[i]=answer[x]+1 #현재 역의 전 역값의+1
+                q.append(i)
+for _ in range(n):
+    a,b=map(int,input().split())
+    station[a].append(b) #인접한 역 설정
+    station[b].append(a)
+    temp[a].append(b) #다음 역 처리를 위해 설정
+    temp[b].append(a)
+    station_size[a]+=1 #간선 수 설정
+    station_size[b]+=1
 while True:
-    flag = True
-    for i in range(N):
-        if graph_size[i] == 1:
-            parent = temp[i].pop()
-            parents[i] = parent
-            temp[parent].remove(i)
-            graph_size[i] = 0
-            graph_size[parent] -= 1
-            flag = False
-
-    if flag:
-        break
-
-BFS()
-
-print(*answer)
+    flag=True
+    for i in range(1,n+1): #간선 수가 1개가 존재하지 않을때까지 반복->순환선만 존재하도록
+        if station_size[i]==1: #간선 수가 1개라는것은 순환선 밖에 제일 마지막 역을 의미
+            next=temp[i].pop() #다음 역 pop->temp리스트 사용 이유:bfs에서 station리스트 사용해야함
+            next_station[i]=next #다음 역 리스트에 삽입
+            temp[next].remove(i) #현재 역과 다음 역과 연결된 간선 삭제
+            station_size[i]=0 #현재 역의 간선 0으로 변경
+            station_size[next]-=1 #다음 역의 간선은 하나 줄으므로 -1
+            flag=False #간선 수 다시 체크
+    if flag: #간선 수 1개인 역이 없으면 순환선만 존재한다는 것으로 break-> 이때 순환선 역의 간선 수는 2개, 순환선이 아닌 역의 간선 수는 0개
+        break 
+bfs() # bfs 사용하여 순환선 아닌 역까지의 거리 구함
+for i in range(1,n+1):
+    print(answer[i],end=' ')
