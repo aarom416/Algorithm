32d30ea1ab6120852ec966deec2a7ab99f30ef4c
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-03-19 15:14:28 +0900

    Update 전화번호 목록.py

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -4,11 +4,12 @@ def solution(phone_book):
     flag=False
     for i in phone_book:
         dic[i]=len(i)
-    for i in phone_book:
-        string=""
+    #하나의 번호에 대해 다른 번호를 비교하는게 아니라(=최대 O(n^2)) 번호의 접두어가 dic에 존재하는지로 시간복잡도 줄임(=최대 O(n*20))
+    for i in phone_book: 
+        string="" #문자 하나씩 받아 접두어를 구성해 dic에 존재하는지 확인할 임의의 문자열 선언
         for j in i:
-            string+=j
-            if string in dic and string!=i:
+            string+=j #하나씩 받기
+            if string in dic and string!=i: #하나씩 받은 접두어가 dic에 존재하고 현재 문자열이 아니면
                 answer=False
                 flag=True
                 break
