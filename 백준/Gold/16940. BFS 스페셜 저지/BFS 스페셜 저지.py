35b8d7fca271294bf70aaee71137ecbb42da5483
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-03-26 11:45:18 +0900

    [Gold III] Title: BFS 스페셜 저지, Time: 416 ms, Memory: 67148 KB -BaekjoonHub

--- 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.py
+++ 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.py
@@ -9,20 +9,21 @@ def bfs():
     q=deque()
     q.append(1)                
     visited[1]=True
-    idx=1
+    idx=1 #자식노드의 첫 인덱스
     while q:
         x=q.popleft()
-        child=[]
+        child=[] #자식노드 리스트
         for i in graph[x]:
             if not visited[i]:
                 visited[i]=True
-                child.append(i)
-        if sorted(ans[idx:idx+len(child)])==sorted(child):
-            for j in ans[idx:idx+len(child)]:
+                child.append(i) #현재 부모 노드에 대한 자식 노드 삽입
+        #답(=ans)에서 자식노드만 slicing을 통해 정렬한 것과 현재 자식 노드를 정렬한 것이 같으면 
+        if sorted(ans[idx:idx+len(child)])==sorted(child): 
+            for j in ans[idx:idx+len(child)]: #다음 자식노드 확인을 위해 q에 삽입
                 q.append(j)
-            idx+=len(child)
+            idx+=len(child) #idx 업데이트
         else:
-            return 0
+            return 0 #같지 않으면 0 리턴
     return 1
 for _ in range(n-1):
     a,b=map(int,input().split())
