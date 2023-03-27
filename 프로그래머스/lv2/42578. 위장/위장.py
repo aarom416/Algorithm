1328792e6543f7e292cfb3ce34e833532fe81094
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-03-27 13:30:13 +0900

    Update 위장.py

--- 프로그래머스/lv2/42578. 위장/위장.py
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -1,14 +1,11 @@
 def solution(clothes):
     answer = 1
-    total=1
     dic={}
-    stack=[]
-    dic[clothes[0][1]]=1
-    for i in range(1,len(clothes)):
+    for i in range(len(clothes)): #종류별로 몇 개 있는지 구분
         if clothes[i][1] not in dic:
             dic[clothes[i][1]]=1
         else:
             dic[clothes[i][1]]+=1
-    for value in dic.values():
-        answer=answer*(value+1)
-    return answer-1
+    for value in dic.values(): #경우의 수 계산
+        answer=answer*(value+1) #+1 는 해당 의상을 사용하지 않는 경우
+    return answer-1 #모든 의상을 사용하지 않는 경우가 있으므로 -1 함
