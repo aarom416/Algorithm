25097fe8013f8d90b2a21ba2be151c8cdbb25583
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-04-05 11:27:21 +0900

    [Gold III] Title: DFS 스페셜 저지, Time: 2732 ms, Memory: 281964 KB -BaekjoonHub

--- 백준/Gold/16964. DFS 스페셜 저지/DFS 스페셜 저지.py
+++ 백준/Gold/16964. DFS 스페셜 저지/DFS 스페셜 저지.py
@@ -1,32 +1,27 @@
 import sys
 from collections import deque
 sys.setrecursionlimit(100000)
-
-
-def dfs(graph, visited, cmp, start):
-    tmp = cmp.popleft()
-    if not cmp:
+input=sys.stdin.readline
+n=int(input())
+graph=[[] for _ in range(n+1)]
+visited=[False]*(n+1)
+def dfs():
+    x=ans.popleft()
+    if not ans:
         print(1)
-        exit(0)
-    visited[tmp] = True
-    for i in range(len(graph[tmp])):
-        if cmp[0] in graph[tmp] and not visited[cmp[0]]:
-            dfs(graph, visited, cmp, cmp[0])
+        exit()
+    visited[x]=True
+    for _ in range(len(graph[x])):
+        if ans[0] in graph[x] and not visited[ans[0]]:
+            dfs()
     return False
-
-
-N = int(sys.stdin.readline())
-visited = [False] * (N + 1)
-graph = [[] for _ in range(N + 1)]
-
-for i in range(1, N):
-    x, y = map(int, sys.stdin.readline().split())
-    graph[x].append(y)
-    graph[y].append(x)
-
-cmp = deque(map(int, sys.stdin.readline().split()))
-if cmp[0] != 1:
-    print(0)
-    exit(0)
-if not dfs(graph, visited, cmp, 1):
+for _ in range(n-1):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+ans=deque(map(int,input().split()))
+if ans[0]==1:
+    if not dfs():
+        print(0)
+else:
     print(0)
