66eb2d4842cea1fc3444914c3b85b9342bb99586
aarom416 <106041072+aarom416@users.noreply.github.com>
2023-06-11 13:16:39 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 352 ms, Memory: 66096 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,5 +1,6 @@
 import sys
 from collections import deque
+sys.setrecursionlimit(1000000)
 input=sys.stdin.readline
 n=int(input())
 graph=[[] for _ in range(n+1)]
@@ -10,16 +11,12 @@ for _ in range(n-1):
     graph[a].append(b)
     graph[b].append(a)
 
-def bfs(x):
-    q=deque([x])
+def dfs(x):
     visited[x]=True
-    while q:
-        x=q.popleft()
-        for i in graph[x]:
-            if not visited[i]:
-                parent_node[i]=x #부모 노드 저장
-                visited[i]=True
-                q.append(i)
-bfs(1)
+    for i in graph[x]:
+        if not visited[i]:
+            parent_node[i]=x #부모 노드 저장
+            dfs(i)
+dfs(1)
 for i in range(2,len(parent_node)):
     print(parent_node[i])
