ec0288e0aa7c5f655136a8cb10db790741b65dbe
aarom416 <106041072+aarom416@users.noreply.github.com>
2024-01-06 14:36:36 +0900

    [Gold V] Title: Contact, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1013. Contact/Contact.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    case = input().strip()
+    result = True
+    while len(case)>0:
+        if case.startswith('100'): #맨앞이 100인 경우
+            case = case[3:] #맨앞 100 제거
+            while len(case)>0 and case.startswith('0'): #100+인 부분에서 0이 연속된 경우 0 제거
+                case = case[1:]
+            
+            if len(case) == 0: #이때 마지막은 무조건 1로 끝나야되는데 100 처럼 끝난 경우 False 기록
+                result = False
+                break
+            
+            case = case[1:] #위에서 100+까지 제거했으므로 100+1인 부분에서 맨앞 1 제거
+            
+            while len(case)>0 and case.startswith('1'): 
+                #100+1+ 부분에서 1001100100 처럼 다시 100이 시작되는 부분을 고려하여 break로 다시 위에서 처리
+                if len(case)>=3 and case[1] == '0' and case[2] == '0': 
+                    break
+                else:
+                    case = case[1:] #10011111 처럼 1 연속되는 부분 1 제거
+                    
+        elif case.startswith('01'): #맨앞이 01인 경우
+            case = case[2:] #맨앞 01 제거 그 후 0110001 같이 100이 시작되는 부분은 위에서 알아서 처리됨
+            
+        else:
+            result = False #그 외에 1100, 1010000 같이 맨앞에 다르 경우는 False 기록
+            break
+        
+    if result:
+        print("YES")
+    else:
+        print("NO")
