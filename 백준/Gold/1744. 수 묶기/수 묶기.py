92f7980d209ffef7d16314beecfcdca79c2c4f49
aarom416 <106041072+aarom416@users.noreply.github.com>
2024-01-07 13:21:12 +0900

    [Gold IV] Title: 수 묶기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -22,20 +22,20 @@ for i in number:
         
 minus_number.sort()
 if len(minus_number) % 2 == 0:
-    for i in range(0,len(minus_number)-1,2):
+    for i in range(0,len(minus_number),2):
         result += minus_number[i] * minus_number[i+1]
 else:
-    for i in range(0,len(minus_number)-2,2):
+    for i in range(0,len(minus_number)-1,2):
         result += minus_number[i] * minus_number[i+1]
     if not exist_zero:
        result += minus_number[-1]
        
 plus_number.sort(reverse=True) 
 if len(plus_number) % 2 == 0:
-    for i in range(0,len(plus_number)-1,2):
+    for i in range(0,len(plus_number),2):
         result += plus_number[i] * plus_number[i+1]
 else:
-    for i in range(0,len(plus_number)-2,2):
+    for i in range(0,len(plus_number)-1,2):
         result += plus_number[i] * plus_number[i+1]
     result += plus_number[-1]
 
