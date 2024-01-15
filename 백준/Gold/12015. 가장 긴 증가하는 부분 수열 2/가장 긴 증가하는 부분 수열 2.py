ab70aca07c9ada20de2e0556bbdbccc39529469b
aarom416 <106041072+aarom416@users.noreply.github.com>
2024-01-15 17:32:14 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 2536 ms, Memory: 155560 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input().strip())
+number_list = list(map(int,input().split()))
+
+result = [number_list[0]]
+
+def binary_search(start, end, target):
+    while start<=end:
+        mid = (start+end)//2
+        if result[mid]==target:
+            return mid
+        elif result[mid] < target:
+            start = mid+1
+        else:
+            end = mid-1
+    return start
+
+for i in range(1,n):
+    if result[-1]<number_list[i]:
+        result.append(number_list[i])
+    else:
+        idx = binary_search(0,len(result)-1,number_list[i])
+        result[idx] = number_list[i]
+print(len(result))
+
