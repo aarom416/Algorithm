016b5fb442917f1b915dc07a20b4c4022f030323
aarom416 <106041072+aarom416@users.noreply.github.com>
2024-01-15 17:54:59 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 2288 ms, Memory: 155552 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -6,24 +6,19 @@ number_list = list(map(int,input().split()))
 
 result = [number_list[0]]
 
-#이분 탐색
 def binary_search(start, end, target):
     while start<=end:
         mid = (start+end)//2
-        if result[mid]==target:
-            return mid
-        elif result[mid] < target:
-            start = mid+1
-        else:
+        if result[mid]>=target:
             end = mid-1
+        else:
+            start = mid+1
     return start
-
+    
 for i in range(1,n):
-    if result[-1]<number_list[i]: #10 20 10 30 20 50 에서 처음 10,20 은 result에 넣음
+    if result[-1]<number_list[i]:
         result.append(number_list[i])
-    else: 
-        # result = [10, 20], number_list = [5, 30, 20, 50] 에서 number_list의 5가 더 작으므로 이분탐색하여 
-        # result의 들어가야할 인덱스를 찾아 넣어줌 5 넣은 결과 => result = [5,20]
+    else:
         idx = binary_search(0,len(result)-1,number_list[i])
         result[idx] = number_list[i]
 print(len(result))
