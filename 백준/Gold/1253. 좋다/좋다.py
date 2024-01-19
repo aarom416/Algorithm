f8222b006392fdeac0bea5d1f9980e8dded82c5e
aarom416 <106041072+aarom416@users.noreply.github.com>
2024-01-19 15:20:08 +0900

    [Gold IV] Title: 좋다, Time: 508 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -6,22 +6,18 @@ n = int(input())
 number_list = list(map(int,input().split()))
 number_list.sort()
 result=0
-#투 포인터 사용
-for i in range(n):
-    target = number_list[i]
+
+def two_pointer(number_list, target):
     start = 0
     end = len(number_list)-1
     while start<end:
         if number_list[start]+number_list[end]==target:
-            if start == i:
-                start += 1
-            elif end == i:
-                end -= 1
-            else:
-                result+=1
-                break
+            return 1
         elif number_list[start]+number_list[end]<target:
-            start += 1
+            start+=1
         else:
-            end -= 1
+            end-=1
+    return 0
+for i in range(n):
+    result+=two_pointer(number_list[:i]+number_list[i+1:], number_list[i])
 print(result)
