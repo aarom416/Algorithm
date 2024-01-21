b2963560655280347aa190f2238d86e9a7341ec0
aarom416 <106041072+aarom416@users.noreply.github.com>
2024-01-21 14:03:14 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 2176 ms, Memory: 155564 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -1,24 +1,25 @@
 import sys
 
-input = sys.stdin.readline
-n = int(input().strip())
+n = int(input())
 number_list = list(map(int,input().split()))
 
-result = [number_list[0]]
+stack = [number_list[0]]
 
-def binary_search(start, end, target):
-    while start <= end:
+def binary_search(target):
+    start=0
+    end=len(stack)-1
+    while start<=end:
         mid = (start+end)//2
-        if result[mid]>=target:
-            end = mid-1
+        if target<=stack[mid]:
+            end = mid - 1
         else:
-            start = mid+1
+            start = mid +1
     return start
-    
+        
 for i in range(1,n):
-    if result[-1]<number_list[i]:
-        result.append(number_list[i])
+    if stack[-1] < number_list[i]:
+        stack.append(number_list[i])
     else:
-        idx = binary_search(0,len(result)-1,number_list[i])
-        result[idx] = number_list[i]
-print(len(result))
+        idx = binary_search(number_list[i])
+        stack[idx] = number_list[i]
+print(len(stack))
