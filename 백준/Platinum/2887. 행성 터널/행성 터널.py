cbc1ecf7ffebdad72619e63922c9afcde28fecf5
aarom416 <106041072+aarom416@users.noreply.github.com>
2024-02-19 15:22:12 +0900

    [Platinum V] Title: 행성 터널, Time: 1196 ms, Memory: 100976 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.py
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -1,4 +1,4 @@
-import sys, heapq, math
+import sys
 
 input = sys.stdin.readline
 
@@ -15,23 +15,33 @@ for i in range(n):
 xList.sort()
 yList.sort()
 zList.sort()
-edges=[[] for _ in range(n)]
+edges=[]
 for curList in xList,yList,zList:
     for i in range(1,n):
         w1,a=curList[i-1]
         w2,b=curList[i]
-        edges[a].append((abs(w1-w2),b))
-        edges[b].append((abs(w1-w2),a))
-heap = [(0,0)]
-visited = [False]*n
+        edges.append((abs(w1-w2),a,b))
+edges.sort(reverse=True)
+parents = [i for i in range(n+1)]
+def find(x):
+    if x != parents[x]:
+        parents[x] = find(parents[x])
+    return parents[x]
+
+def union(x,y):
+    x = find(x)
+    y = find(y)
+    if x<y:
+        parents[y]=x
+    else:
+        parents[x]=y
+
 count=0
 result=0
-while count!=n:
-    w,s= heapq.heappop(heap)
-    if not visited[s]:
-        visited[s]=True
+while count!=n-1:        
+    w,a,b = edges.pop()
+    if find(a) != find(b):
+        union(a,b)
         count+=1
         result+=w
-        for i in edges[s]:
-            heapq.heappush(heap,i)
 print(result)
