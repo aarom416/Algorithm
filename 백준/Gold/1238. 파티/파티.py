01476c2611064b1039524cedd758291a62f359cb
aarom416 <106041072+aarom416@users.noreply.github.com>
2024-03-02 22:35:27 +0900

    Update 파티.py

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -23,6 +23,7 @@ def dijkstra(start):
                 dist[i[0]] = cost
                 heapq.heappush(heap,(cost,i[0]))
 
+#집에서 목적지 가는 거리를 반환하는 다익스트라 알고리즘
 def go(start,x):
     heap = []
     dist = [INF] * (n+1)
@@ -42,10 +43,13 @@ come_roads = [0] * (n+1)
 for _ in range(m):
     a,b,x = map(int,input().split())
     roads[a].append((b,x))
+#목적지에서 각 집으로 가는데 최단 거리 구해 저장
 dijkstra(start)
+#각 집에서 목적지까지 가는데 최단 거리 구해 저장
 for i in range(1,n+1):
     come_roads[i] = go(i,start)
 max_dist = 0
+#목적지 갔다 오는 거리의 최대값 비교하며 갱신
 for i in range(1,len(dist)):
     max_dist = max(max_dist, dist[i]+come_roads[i])
 print(max_dist)
