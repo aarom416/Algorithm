7d8f07e90b9d33d27785c1a0814a6d94b2ec3687
aarom416 <106041072+aarom416@users.noreply.github.com>
2026-09-18 02:39:01 +0900

    [level 2] Title: 전화번호 목록, Time: 442.06 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,14 @@
-def solution(phoneBook):
-    phoneBook = sorted(phoneBook)
+from collections import Counter
 
-    for p1, p2 in zip(phoneBook, phoneBook[1:]):
-        if p2.startswith(p1):
-            return False
+def solution(phone_book):
+    
+    dic = Counter(phone_book)
+    
+    for phone_number in phone_book:
+        temp = ''
+        for p in phone_number:
+            temp += p
+            if temp in dic and temp != phone_number:
+                return False
+
     return True
