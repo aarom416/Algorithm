a00d0ca90eac4a2992f8f2dd9c0cab70528e3280
aarom416 <106041072+aarom416@users.noreply.github.com>
2026-09-21 23:53:09 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,8 +1,9 @@
 
 def solution(citations):
-    citations.sort(reverse=True)
-    h_index = 0
-    for c in citations:
-        if c > h_index:
-            h_index += 1
-    return h_index
+    citations.sort()
+    l = len(citations)
+    
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
