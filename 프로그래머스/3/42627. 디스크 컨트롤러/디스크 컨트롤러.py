4dc4f87e31b72aee9b4139a0edeb85e6b480ca59
aarom416 <106041072+aarom416@users.noreply.github.com>
2026-09-22 19:09:07 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.43 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,39 +1,21 @@
+from collections import deque
 import heapq as hq
 
 def solution(jobs):
     
-    total_jobs_count = len(jobs)
-    jobs.sort(key=lambda x:x[0], reverse=True)
-    waiting_heap = []
-    current_time = 0
-    start_time = 0
-    end_time = 0
-    processing_time = 0
-    request_time = 0
-    response_list = []
-    is_processing = False
-    while jobs or waiting_heap or is_processing:
-        
-        # 대기 큐에 넣기 -> 소요시간 먼저 우선순위
-        while jobs and current_time == jobs[-1][0]:
-            r, p = jobs.pop()
-            hq.heappush(waiting_heap, [p,r])
-            
-        # 작업 완료
-        if is_processing and (current_time-start_time == processing_time):
-            end_time = current_time
-            is_processing = False
-            response_list.append(end_time - request_time)
-            
-        # 작업 시작
-        if not is_processing and waiting_heap:
-            p,r = hq.heappop(waiting_heap)
-            processing_time = p
-            request_time = r
-            start_time = current_time
-            is_processing = True
-            
-        
-        current_time += 1
-        
-    return sum(response_list)//total_jobs_count
+    total_jobs = len(jobs)
+    tasks = sorted([(x[1], x[0]) for x in jobs], key = lambda x: (x[1], x[0]), reverse = True)
+    
+    q = []
+    hq.heappush(q, tasks.pop())
+    current_time, total_response_time = 0,0
+    
+    while q:
+        p, r = hq.heappop(q)
+        current_time = max(p+r, current_time+p)
+        total_response_time += current_time-r
+        while tasks and tasks[-1][1] <= current_time:
+            hq.heappush(q, tasks.pop())
+        if tasks and not q:
+            hq.heappush(q, tasks.pop())
+    return total_response_time // total_jobs
