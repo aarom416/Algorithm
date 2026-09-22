70d4369e44287179893b8900143c153e8d7899bd
aarom416 <106041072+aarom416@users.noreply.github.com>
2026-09-23 01:24:26 +0900

    Update 모의고사.py

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -30,3 +30,26 @@ def solution(answers):
             result.append(i+1)
             
     return result
+
+# 다른 풀이
+def solution(answers):
+    answer = []
+    
+    first_pattern = [1,2,3,4,5] 
+    second_pattern = [2,1,2,3,2,4,2,5] 
+    third_pattern = [3,3,1,1,2,2,4,4,5,5] 
+    score = [0,0,0]
+    
+    for i, a in enumerate(answers):
+        if a == first_pattern[i%len(first_pattern)]:
+            score[0] += 1
+        if a == second_pattern[i%len(second_pattern)]:
+            score[1] += 1
+        if a == third_pattern[i%len(third_pattern)]:
+            score[2] += 1
+        
+    for i, s in enumerate(score):
+        if s == max(score):
+            answer.append(i+1)
+            
+    return answer
