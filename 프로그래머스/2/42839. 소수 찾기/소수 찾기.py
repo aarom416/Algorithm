dfe8989578aaa71be8ffc58bb1e7ebd6a90c4c8a
aarom416 <106041072+aarom416@users.noreply.github.com>
2026-09-23 15:14:58 +0900

    Update 소수 찾기.py

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -22,3 +22,30 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+
+## 다른 풀이 백트리캥 + 슬라이싱
+number_set = set()
+
+def solution(numbers):
+    
+    makeCombinations("", numbers)
+    
+    answer = len(number_set)
+    
+    return answer
+
+def makeCombinations(str1, str2):
+    if str1 and is_prime(int(str1)):
+        number_set.add(int(str1))
+        
+    for i in range(len(str2)):
+        makeCombinations(str1 + str2[i], str2[:i] + str2[i+1:])
+        
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
