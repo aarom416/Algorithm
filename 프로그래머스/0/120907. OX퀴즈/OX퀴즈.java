d15f27292a08bd5d8902ffacc402ce02f017fb61
aarom416 <106041072+aarom416@users.noreply.github.com>
2026-09-24 16:34:38 +0900

    [level 0] Title: OX퀴즈, Time: 0.20 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        
+        for (int i = 0; i < quiz.length; i++) {
+            String[] s = quiz[i].split(" ");
+            
+            if (s[1].equals("+")) {
+                if (Integer.parseInt(s[0]) + Integer.parseInt(s[2]) == Integer.parseInt(s[4])) {
+                    answer[i] = "O";
+                } else {
+                    answer[i] = "X";
+                }
+            } else {
+                if (Integer.parseInt(s[0]) - Integer.parseInt(s[2]) == Integer.parseInt(s[4])) {
+                    answer[i] = "O";
+                } else {
+                    answer[i] = "X";
+                }
+            }
+        
+        }
+        
+        return answer;
+    }
+}
