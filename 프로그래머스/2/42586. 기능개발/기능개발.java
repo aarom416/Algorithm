15b67516488cb15dd468d711d01e705882142625
aarom416 <106041072+aarom416@users.noreply.github.com>
2026-09-26 19:38:23 +0900

    Update 기능개발.java

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -37,3 +37,36 @@ class Solution {
             .toArray();
     }
 }
+
+# 다른 풀이 - Queue 자료구조 이용
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> answerList = new ArrayList();
+        
+        for (int i=0; i<speeds.length; i++) {
+            double last = (100-progresses[i])/ (double) speeds[i];
+            int lastDay = (int) Math.ceil(last);
+            
+            if (!q.isEmpty() && q.peek() < lastDay) {
+                answerList.add(q.size());
+                q.clear();
+            }
+            
+            q.offer(lastDay);
+        }
+        
+        answerList.add(q.size());
+        
+        int[] answer = new int[answerList.size()];
+        
+        for (int i=0; i<answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+        
+        return answer;
+    }
+}
